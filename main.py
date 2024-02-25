ee6d8e3a85a27331010f67b7c6c8f4f48133bad5
bahrom04 <magdiyevbahrom@gmail.com>
2024-02-25 21:44:20 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+ask = input("How can I help you: ")
+
+while True:
+    if ask == "Hi":
+        print("Hi")
+    if ask == "Hello":
+        print("Hello there")
+    if ask == "Sup":
+        print("Hey there!")
+
+    ask = input("How can I help you: ")
